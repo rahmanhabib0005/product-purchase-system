07bbbed8a631226f9e87f8d9443623535cdd88b8
rahmanhabib0005 <rahmanhabib0005@gmail.com>
2025-02-22 18:29:52 +0600

    Add 2 middleware

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,13 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Auth::routes();
+Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login')->middleware('guest');
+Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login'])->middleware('guest');
+Route::post('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout')->middleware('auth');
 
 Route::prefix('admin')->middleware(['auth'])->as('admin.')->group(function () {
     Route::resource('products', App\Http\Controllers\Admin\ProductController::class)->names('product');
